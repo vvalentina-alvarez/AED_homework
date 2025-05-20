ef4b4be8b46f50b6980f349ed6b16591779409bb
valentina alvarez <valentina.alvarez@utec.edu.pe>
2025-05-20 10:59:36 -0500

    Create bst.cpp

---
+++ bst.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+
+
+
+class Solution {
+public:
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+        return helper(root).first;
+    }
+    
+private:
+    pair<TreeNode*, int> helper(TreeNode* node) {
+        if (!node) return {nullptr, 0};
+        
+        auto left = helper(node->left);
+        auto right = helper(node->right);
+        
+        if (left.second > right.second) {
+            return {left.first, left.second + 1};
+        }
+        if (left.second < right.second) {
+            return {right.first, right.second + 1};
+        }
+        
+        // Si ambas profundidades son iguales, el LCA es el nodo actual
+        return {node, left.second + 1};
+    }
+};
